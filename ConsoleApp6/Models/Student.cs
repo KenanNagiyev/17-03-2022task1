9f87b423542a57f707a13f591d39eea38b163f5f
Kanan Naghiyev <tu788lnmp@aztu.local>
2022-03-17 15:00:22 +0400

    new file

---
+++ ConsoleApp6/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6.Models
+{
+     class Student:Person
+    {
+       public void Semi()
+        {
+            Name = "Semseddin";
+            SurName = "Amanov";
+            Age = 20;
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary1;
+using ConsoleApp6.Models;
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            Teacher t = new Teacher();
+            Student stud = new Student();
+            Console.WriteLine(stud.Age);
+            Console.ReadLine();
+        }
+    }
+}
