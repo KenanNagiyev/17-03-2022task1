c6ae0bc6e57aef1fd555eff901a58450937d0283
Kanan Naghiyev <tu788lnmp@aztu.local>
2022-03-17 19:48:59 +0400

    new file

---
+++ ClassLibrary1/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ ClassLibrary1/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Teacher:Person
+    {
+        public void Kenan()
+        {
+            Name = "Kenan";
+            SurName = "Nagiyev";
+            Age = 20;
+        }
+    }
+}
